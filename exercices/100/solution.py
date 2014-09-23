790bd4931b40c9703075a674c8b7ccb96840edaa
Louis Halconruy <l.halconruy@gmail.com>
2014-09-23 12:04:29 +0200

    100

---
+++ exercices/100/solution.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 23 11:37:26 2014
+
+@author: Louis
+"""
+
+station = {
+    'address': 'RUE DES CHAMPEAUX (PRES DE LA GARE ROUTIERE) - 93170 BAGNOLET',
+    'number': 31705,
+    'latitude': 48.8645278209514,
+    'name': 'CHAMPEAUX (BAGNOLET)',
+    'longitude': 2.416170724425901
+}
+
+print(station['address'])
